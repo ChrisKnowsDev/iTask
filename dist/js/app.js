4cfd67fc5a4a9c384ad9b020cd358387124c65fc
chrisharley81 <chrisharley81@gmail.com>
2020-01-18 09:17:38 -0500

    task added to UI on enter press

--- dist/js/app.js
+++ dist/js/app.js
@@ -17,12 +17,54 @@ addIcon.addEventListener('click', addTask);
 function addTask() {
   addIcon.style.display = 'none';
   addInput.style.display = 'inline-block';
+  addInput.focus();
 
   // Listen for user submit on add task input
-  addInput.addEventListener('keyup', function(e) {
-    if (e.keyCode === 13) {
-      console.log(addInput.value);
-      e.preventDefault();
-    }
-  });
+  addInput.addEventListener('keyup', getUserSubmit);
+}
+
+// Get user submit
+function getUserSubmit(e) {
+  let userInput = addInput.value;
+  // check if user presses enter
+  if (e.keyCode === 13) {
+    addInput.value = '';
+    addIcon.style.display = 'inline-block';
+    addInput.style.display = 'none';
+
+    // Create li and inner html
+    createLi(userInput);
+
+    e.preventDefault();
+  }
+}
+
+// Create Li
+function createLi(value) {
+  // Creates li and inner html links/icons
+  const li = document.createElement('li'),
+    liInput = document.createElement('input'),
+    editLink = document.createElement('a'),
+    editIcon = document.createElement('i'),
+    deleteLink = document.createElement('a'),
+    deleteIcon = document.createElement('i');
+
+  // Add classes to icons
+  editIcon.classList.add('fas', 'fa-pencil-alt');
+  deleteIcon.classList.add('fas', 'fa-trash');
+
+  // Append icons to link
+  editLink.appendChild(editIcon);
+  deleteLink.appendChild(deleteIcon);
+
+  // Append input value to li
+  liInput.value = value;
+  li.appendChild(liInput);
+
+  // Append links to li
+  li.appendChild(editLink);
+  li.appendChild(deleteLink);
+
+  // Append li to list
+  list.appendChild(li);
 }
